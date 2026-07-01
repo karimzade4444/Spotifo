6fad3ee869a615d87a87936ef7f0bc42fb15368c
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-02 00:55:29 +0500

    api

--- src/types/overload.types.ts
+++ src/types/overload.types.ts
@@ -1,3 +1,9 @@
-import type { ITrack } from "./music.types";
+import type { IArtist, ITrack } from "./music.types";
+import type { IPlaylist } from "./playlist.types";
 
 function spotifoApi(endpoint: 'track', id: string): Promise<ITrack>;
+function spotifoApi(endpoint: 'artist', id: string): Promise<IArtist>;
+function spotifoApi(endpoint: 'playlist', id: string): Promise<IPlaylist>;
+function spotifoApi(endpoint: string, id: string): Promise<unknown>{
+    return fetch(`https://api.spotifo.com/${endpoint}/${id}`).then(res=>res.json());
+}
