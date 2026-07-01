6a8356f45b4870d55c487516b8d306f9a5a3ab4f
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-01 23:43:24 +0500

    artist

--- src/types/music.types.ts
+++ src/types/music.types.ts
@@ -1,6 +1,17 @@
-type TrackId = string;
+type ID = string;
 type Duration = number;
 type ImageUrl = string;
 type isPublished = boolean;
 type artist = null | string;
 type album = undefined | string;
+
+interface IArtist {
+id: ID,
+name: string,
+image: ImageUrl;
+isVerifed: boolean
+}
+
+interface ITrack {
+    id: ID,
+}
