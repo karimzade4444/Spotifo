8cb4bd997de1415111ab3290886ba50bea74a0cd
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-02 00:46:02 +0500

    volume

--- src/types/fn.types.ts
+++ src/types/fn.types.ts
@@ -6,9 +6,18 @@
  console.log(`Playing track: ${track}`)
  }
 
+ /*
  function setVolume(level: number): void {
    if (level < 0 || level > 100) {
      throw new Error('Volume level must be between 0 and 100.')
    } 
     console.log(`Setting volume to: ${level}`);
+}
+    */
+
+const setVolume: TVolumeFunction = function (level) {
+  if (level < 0 || level > 100) {
+    throw new Error('Volume level must be between 0 and 100.');
+  }
+  console.log(`Setting volume to: ${level}`);
 }
