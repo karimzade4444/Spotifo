c8afc0e89e9a0a643e19522e8334bea3fe814ff5
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-02 15:18:23 +0500

    enum

--- src/types/enum.types.ts
+++ src/types/enum.types.ts
@@ -1,7 +1,20 @@
-enum AudioQuality {
+enum EnumAudioQuality {
     LOW = "96",
     MEDIUM = "192",
     HIGH = "320",
     LOSSLESS = "FLAC",
 }
 
+export const AudioQuality = {
+    LOW: "96",
+    MEDIUM: "192",
+    HIGH: "320",
+    LOSSLESS: "FLAC",
+} as const;
+
+export type AudioQuality = typeof AudioQuality[keyof typeof AudioQuality];
+
+let q: AudioQuality;
+ q = AudioQuality.HIGH
+ q= '320'
+ console.log(q)
