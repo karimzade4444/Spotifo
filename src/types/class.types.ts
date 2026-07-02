9a873a6a4197e6bdcb92aab4226de31a0941adcc
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-02 15:04:32 +0500

    fix

--- src/types/class.types.ts
+++ src/types/class.types.ts
@@ -1,40 +1,39 @@
-import { Howl } from 'howler' 
-import type { IPlayerState } from './player.types';
-import type { ITrack } from './music.types';
-
+import { Howl } from "howler";
+import type { IPlayerState } from "./player.types";
+import type { ITrack } from "./music.types";
 
 export class SporifoPlayer {
-    private audio: Howl | null = null;
-    private state: IPlayerState
+  private audio: Howl | null = null;
+  private state: IPlayerState;
 
-    constructor(){
+  constructor() {
     this.state = {
-        currentTrack: null,
-        queue: [],
-        history: [],
-        isPlaying: false,
-        volume: 0.7,
-        currentTime: 0,
-        repeatMode: 'off',
-        isShuffling: false,
-    }
-}
-play(track?:ITrack): void {
+      currentTrack: null,
+      queve: [],
+      history: [],
+      isPlaying: false,
+      volume: 0.7,
+      currentTime: 0,
+      repeatMode: "off",
+      isShuffling: false,
+    };
+  }
+  play(track?: ITrack): void {
     if (track) {
-        this.loadTrack(track);
+      this.loadTrack(track);
     }
     this.audio?.play();
     this.state.isPlaying = true;
-}
+  }
 
-private loadTrack(track: ITrack): void {
+  private loadTrack(track: ITrack): void {
     this.audio = new Howl({
-        src: [track.audioUrl],
-        volume: this.state.volume
-    })
+      src: [track.audioUrl],
+      volume: this.state.volume,
+    });
     this.state.currentTrack = track;
-}
-seek(time: number): void {
+  }
+  seek(time: number): void {
     this.audio?.seek(time);
-}
+  }
 }
