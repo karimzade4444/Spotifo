f0968689260defeda32fd81f3156b7b791de4c9f
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-02 14:56:48 +0500

    play

--- src/types/class.types.ts
+++ src/types/class.types.ts
@@ -1,5 +1,24 @@
-import { Howl } from "howler";
+import { Howl } from 'howler' 
+import type { IPlayerState } from './player.types';
+import type { ITrack } from './music.types';
 
 
 export class SporifoPlayer {
-    private audio: Howl
+    private audio: Howl | null = null;
+    private state: IPlayerState
+
+    constructor(){
+    this.state = {
+        currentTrack: null,
+        queue: [],
+        history: [],
+        isPlaying: false,
+        volume: 0.7,
+        currentTime: 0,
+        repeatMode: 'off',
+        isShuffling: false,
+    }
+}
+play(track?:ITrack): void {
+    if (track) {
+        this.loadTrack(track);
